c2ecad8458a0f36a2a9f191afa0da04d8b8f4fca
Fernando Naon <74456529+FernandoNaon@users.noreply.github.com>
2022-02-14 23:28:58 -0300

    test salesHistory dispatch order

--- src/components/SalesHistory/SalesHistory.js
+++ src/components/SalesHistory/SalesHistory.js
@@ -17,10 +17,10 @@ const SalesHistory = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    dispatch(getAllTickets());
     dispatch(allShows());
     dispatch(theaterDetail(id));
-  }, []);
+    dispatch(getAllTickets());
+  }, [dispatch, id]);
 
   let filterShows = show?.filter((e) => e.theaterId === theater?.id);
   let filterTicket = tickets?.filter((e) => e.show.theaterId === theater?.id);
